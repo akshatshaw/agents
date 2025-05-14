fbbbe0f1b36e31a7b3e837b9dd2b4b6ff49fa723
akshatshaw <akshaw.ak4@gmail.com>
2025-05-14 20:30:27 +0530

    path update

--- nl2sql_agent/app/utils.py
+++ nl2sql_agent/app/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 import asyncio
 from typing import List, Dict, Any
-df = pd.read_csv(r"C:\Users\AKSHAT SHAW\OneDrive - iitr.ac.in\Desktop\Side-Projects\Agents\nl2sql_agent\Copy of Schema_dump_(1)(1).csv")
+df = pd.read_csv(r"nl2sql_agent\Copy of Schema_dump_(1)(1).csv")
     
 
 SCHEMA_SAMPLES = {}
